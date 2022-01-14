9861cb0e6aaaffd8565ef943ed6b9db9af431674
Foopy969 <33833930+Foopy969@users.noreply.github.com>
2022-01-14 18:27:59 +0800

    Update makedata.py
    compatibility update for new pdf

--- scripts/makedata.py
+++ scripts/makedata.py
@@ -6,28 +6,19 @@ f=open('MTT_2122S1_ASc2_V1.pdf', 'rb')
 fr=PdfFileReader(f)
 
 for i in range(fr.getNumPages()):
-    t=fr.getPage(i).extractText().partition('TimeRoom')[2].partition('\nBuilding code' if i == 0 else '\nLast updated')[0].split('\n')
-
-    for j in range(len(t)):
-        if len(t[j]) == 8:
-            t[j] += t[j+1]
-            t[j+1] = ''
-    t=[j for j in t if j != '']
-
-    for j in range(len(t)):
-        if t[j][0].isdigit():
-            t[j-1] += t[j]
-            t[j] = ''
-    t=[j for j in t if j != '']
-        
+    t=fr.getPage(i).extractText().partition('Room\n')[2].partition('\nBuilding code' if i == 0 else '\nLast updated')[0].split('\n')
     a.extend(t)
 
-for i in a:
-    s=[j[::-1] for j in i[::-1].split(' - ', 1)]
-    if i[:8] not in o:
-        o[i[:8]]=[s[1][12:-7]]
-    o[i[:8]].extend([i[8:12], s[1][-6], s[1][-5:], s[0][5:]])
+    for j in range(len(a)):
+        if a[j][0] == '(':
+            a[j-1] += a[j]
+            a[j] = ''
+    a=[j for j in a if j != '']
+
+for i in [a[i:i+7] for i in range(0,len(a),7)]:
+    if i[0] not in o.keys():
+        o[i[0]]=i[1:]
+    o[i[0]].extend(i[1:])
 
 with open('data.json', 'w+') as f:
     dump(o, f)
-
